7967f0a7af8716180d9d2efc0758e6a2e17d9f7a
Nicholas Miller <nlm82@uakron.edu>
2022-09-27 11:27:40 -0400

    Add user table to djangodb

--- djangodb/inventory/models.py
+++ djangodb/inventory/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.firstname + ' ' + self.lastname
+  
